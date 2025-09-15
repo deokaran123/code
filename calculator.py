7e724f96e5061a250c6541c16268f4a5db429c9d
deokaran123 <deokaran2005@gmail.com>
2025-09-15 19:06:16 +0530

    calculator using eval

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# building a calculator using the inbuilt function of python
+n = input("Enter the expression: ")
+try: 
+    print(eval(n))
+except SyntaxError :
+    print("Please write in correct syntax")
+except ZeroDivisionError:
+    print("Dividing any number by zero is infinity")
+except NameError:
+    print("Only Integer values are allowed")
+
